922d3c7a924024a96f5a4066d2628ee4e191e1f8
makafuifie <fiemakafui2013@gmail.com>
2016-11-29 12:28:13 +0000

    first

---
+++ js/index.js
@@ -0,0 +1,13 @@
+(function() {
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  }
+  );
+		$('.collapsible').collapsible();
+	
+})();
+
+(function(){
+	
+})
---
+++ setting.php
@@ -0,0 +1,18 @@
+<?php
+define("DB_HOST","localhost");
+//define("DB_HOST","localhost");
+define("DB_USERNAME","makafui.fie");
+define("DB_PASSWORD","bcde82a208262cd2");
+
+define("DB_NAME","db__makafui_fie");
+
+?>
+
+<?php
+// define("DB_HOST","localhost");
+// define("DB_USERNAME","root");
+// define("DB_PASSWORD","");
+
+// define("DB_NAME","car_pool");
+
+?>
