59578aab0f426cd1d07862fb63fcbd2d7959d1af
Makafui Fie <fiemakafui2013@gmail.com>
2016-12-04 01:10:55 +0000

    Revert "login interface"
    This reverts commit 176e6676d71c6d91cd3f63d6fb6b97b99dcd4dc9.

--- script.sql
+++
@@ -1,9 +0,0 @@
-create TABLE users(
-  userID int not null AUTO_INCREMENT,
-  firstname varchar(50),
-    lastname varchar(50),
-    email varchar(100),
-    password varchar(20),
-    phoneNumber varchar(15),
-    primary key(userID)  
-);
