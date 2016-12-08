f33550d61bb81cece34cae27d08d53bdd58d7f6e
Makafui Fie <fiemakafui2013@gmail.com>
2016-12-08 02:54:52 +0000

    directory

--- script.sql
+++ script.sql
@@ -16,3 +16,12 @@ create TABLE admin(
     phoneNumber varchar(15),
     primary key(adminID)  
 );
+
+CREATE TABLE `markers` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `name` VARCHAR( 60 ) NOT NULL ,
+  `address` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL ,
+  `type` VARCHAR( 30 ) NOT NULL
+);
